369672561a85b289f1de965e4818b9d7e5b0a4a2
maestroanth <maestroanth@gmail.com>
2017-03-06 21:52:41 -0700

    Add project files.

---
+++ NetDoodler/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NetDoodler.MvcApplication" Language="C#" %>
---
+++ NetDoodler/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NetDoodler.Startup))]
+namespace NetDoodler
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
